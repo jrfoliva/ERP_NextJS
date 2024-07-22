0da9b6b624c3b973c7cb2a5d73fb1ffefe97e29e
Junior Oliva <jrfoliva@gmail.com>
2024-07-22 14:27:24 -0300

    Primeiro Commit

---
+++ app/page.js
@@ -0,0 +1,25 @@
+import LikeButton from "./like-button";
+
+function Header({ title }) {
+    return (
+        <h1>{title ? title : 'Default title'}</h1>
+    )
+}
+
+function HomePage() {
+    const names = ['Junior', 'Veruska', 'Arhtur'];
+
+    return (
+        <div>
+            <Header title="Develop. Preview. Ship" />
+            <ul>
+                {names.map((name) =>
+                    <li key="name">{name}</li>
+                )}
+            </ul>
+            <LikeButton />
+        </div>
+    )
+}
+
+export default HomePage;
